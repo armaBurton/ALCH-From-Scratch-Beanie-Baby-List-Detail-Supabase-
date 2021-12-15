3e0143375051672867cb8472704f80beb9535ba5
armaBurton <armanarma@armaburton.com>
2021-12-15 11:20:28 -0800

    pages change, id is gathered from url

--- detail/script/detail-script.js
+++ detail/script/detail-script.js
@@ -3,6 +3,9 @@ import { renderDetailCard } from './render-detail-utils.js'
 
 window.addEventListener(`load`, async() => {
     const card = await getDetailCard();
-    console.log(card[0]);
-    renderDetailCard(card[0]);
+    // const url = new URL(location.href);
+    // const search = url.searchParams;
+    // card.id = search.get(`id`);
+    // console.log(card.id);
+    renderDetailCard(card);
 });
